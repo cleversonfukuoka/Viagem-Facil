9e298b03c6aaf6b5bcb7c9fecd48798fe712bbe0
Fukuoka, Cleverson <cleversonfukuoka@hotmail.com>
2024-12-08 22:31:29 -0300

    Initial Commit

---
+++ Viagem Facil/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Viagem Facil
+//
+//  Created by Cleverson Fukuoka on 08/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Viagem Facil/Viagem_FacilApp.swift
@@ -0,0 +1,17 @@
+//
+//  Viagem_FacilApp.swift
+//  Viagem Facil
+//
+//  Created by Cleverson Fukuoka on 08/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Viagem_FacilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
